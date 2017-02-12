54c3031f567060c230c42ee5038e2724c76b7962
cosmic <nayem.cosmic@gmail.com>
2017-02-13 00:38:18 +0600

    foil.py edited.

--- foil/foil.py
+++ foil/foil.py
@@ -12,12 +12,12 @@ ax = fig.add_subplot(111, projection='3d')
 f = open("foil_mesh.scr", 'w')
 
 
-c = .2
-s = 1.
+c = 10.
+s = 80.
 m = .04
 p = .4
 t = .12
-ib = 50
+ib = 20
 jb = 10
 
 dx = c/ib
@@ -40,13 +40,13 @@ for j in range(jb+1):
                 zc=m*(c-x)*(1+x/c-2*p)/((1-p)**2)
 
 
-            zt = 5*t*(.2969*np.sqrt(x/c)-.1260*(x/c)-.3516*np.power(x/c,2)+.2843*np.power(x/c,3)-.1015*np.power(x/c,4))
+            zt = 5*t*c*(.2969*np.sqrt(x/c)-.1260*(x/c)-.3516*np.power(x/c,2)+.2843*np.power(x/c,3)-.1015*np.power(x/c,4))
             xu = x-zt*np.sin(np.arctan(grad))
             zu = zc+zt*np.cos(np.arctan(grad))
             xco.append(xu)
             yco.append(y)
             zco.append(zu)
-    for i in range(ib,-1,-1):
+    for i in range(ib-1,-1,-1):
              x = dx*i
              if x<p*c:
                  grad = 2*m*(p-x/c)/p**2
@@ -56,12 +56,39 @@ for j in range(jb+1):
                  grad = 2*m*(p-x/c)/np.power((1-p), 2)
                  zc=m*(c-x)*(1+x/c-2*p)/(1-p)**2
              
-             zt = 5*t*(.2969*np.sqrt(x/c)-.1260*(x/c)-.3516*np.power(x/c,2)+.2843*np.power(x/c,3)-.1015*np.power(x/c,4))
+             zt = 5*t*c*(.2969*np.sqrt(x/c)-.1260*(x/c)-.3516*np.power(x/c,2)+.2843*np.power(x/c,3)-.1015*np.power(x/c,4))
              xl = x+zt*np.sin(np.arctan(grad))
              zl = zc-zt*np.cos(np.arctan(grad))
              xco.append(xl)
              yco.append(y)
-             zco.append(zl) 
+             zco.append(zl)
+
+#following part is for PFACE command in autocad. It is needed in foil_mesh.scr 
+f.truncate()
+f.write("PFACE\n")
+i=0
+for data in xco:
+    f.write(str(xco[i]))
+    f.write(",")
+    f.write(str(yco[i]))
+    f.write(",")
+    f.write(str(zco[i]))
+    f.write("\n")
+    i=i+1
+
+for i in range(1,(ib*2+1)*jb+1,1):
+    f.write("\n")
+    f.write(str(i))
+    f.write("\n")
+    f.write(str(i+1))
+    f.write("\n")
+    f.write(str(i+2*ib+2))
+    f.write("\n")
+    f.write(str(i+2*ib+1))
+    f.write("\n")
+f.close()
+   
+ 
 for i in range(ib+1):
     x = dx*i
     if x<p*c:
@@ -71,7 +98,7 @@ for i in range(ib+1):
         grad = 2*m*(p-x/c)/(1-p)**2
         zc=m*(c-x)*(1+x/c-2*p)/(1-p)**2
  
-    zt = 5*t*(.2969*np.sqrt(x/c)-.1260*(x/c)-.3516*np.power(x/c,2)+.2843*np.power(x/c,3)-.1015*np.power(x/c,4))
+    zt = 5*t*c*(.2969*np.sqrt(x/c)-.1260*(x/c)-.3516*np.power(x/c,2)+.2843*np.power(x/c,3)-.1015*np.power(x/c,4))
     xu = x-zt*np.sin(np.arctan(grad))
     zu = zc+zt*np.cos(np.arctan(grad))
     if i%2==0:
@@ -95,7 +122,7 @@ for i in range(ib,-1,-1):
         grad = 2*m*(p-x/c)/(1-p)**2
         zc=m*(c-x)*(1+x/c-2*p)/(1-p)**2
  
-    zt = 5*t*(.2969*np.sqrt(x/c)-.1260*(x/c)-.3516*np.power(x/c,2)+.2843*np.power(x/c,3)-.1015*np.power(x/c,4))
+    zt = 5*t*c*(.2969*np.sqrt(x/c)-.1260*(x/c)-.3516*np.power(x/c,2)+.2843*np.power(x/c,3)-.1015*np.power(x/c,4))
     xl = x+zt*np.sin(np.arctan(grad))
 
     zl = zc-zt*np.cos(np.arctan(grad))
@@ -122,18 +149,4 @@ plt.xlabel('Chordwise Length')
 plt.ylabel('Spanwise Length')
 
 savefig('foil_mesh.png')
-
-f.truncate()
-f.write("PFACE\n")
-i=0
-for data in xco:
-    f.write(str(xco[i]))
-    f.write(",")
-    f.write(str(yco[i]))
-    f.write(",")
-    f.write(str(zco[i]))
-    f.write("\n")
-    i=i+1
-f.close()
-
 plt.show()
